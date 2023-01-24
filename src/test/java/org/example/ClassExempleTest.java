b20dd31219155ccb76a39930716934bd5bd60bd3
JordaneBuena <121666719+JordaneBuena@users.noreply.github.com>
2023-01-24 15:37:50 +0100

    Add sans erreur

--- src/test/java/org/example/ClassExempleTest.java
+++ src/test/java/org/example/ClassExempleTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClassExempleTest {
     @Test
     void classExemple(){
-        final int NOMBRE =3;
-        ClassExemple classExemple = new ClassExemple(5);
+        final int NOMBRE =5;
+        ClassExemple classExemple = new ClassExemple(NOMBRE);
         assertEquals(NOMBRE, classExemple.getNumber()); //TRUE si les 2 sont pareils, false si les 2 sont différents
 
 }
